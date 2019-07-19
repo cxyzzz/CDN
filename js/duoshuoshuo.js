0e74ee11c4b88603019359275d8dcbe01e12a910
cxyzzz <2283361094@qq.com>
2019-07-19 20:28:13 +0800

    fix custom script

--- js/duoshuoshuo.js
+++ js/duoshuoshuo.js
@@ -1,3 +1,4 @@
+<!--崩溃欺骗-->
 var OriginTitile = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
@@ -8,22 +9,9 @@ var OriginTitile = document.title;
      }
      else {
          $('[rel="icon"]').attr('href', "/images/favicon-16x16.png");
-         document.title = '(ฅ>ω<script>
-        document.querySelectorAll('.github-emoji')
-          .forEach(el => {
-            if (!el.dataset.src) { return; }
-            const img = document.createElement('img');
-            img.style = 'display:none !important;';
-            img.src = el.dataset.src;
-            img.addEventListener('error', () => {
-              img.remove();
-              el.style.color = 'inherit';
-              el.style.backgroundImage = 'none';
-              el.style.background = 'none';
-            });
-            img.addEventListener('load', () => {
-              img.remove();
-            });
-            document.body.appendChild(img);
-          });
-      </script></body></html>
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
